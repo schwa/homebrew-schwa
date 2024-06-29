a469ca99e3b69b34adcc562250806277a48bfdb3
Jonathan Wight <jwight@mac.com>
2024-06-29 10:16:10 -0400

    Fix url.

--- Formula/swiftlint-autodetect.rb
+++ Formula/swiftlint-autodetect.rb
@@ -4,7 +4,7 @@
 class SwiftLintAutodetect < Formula
     desc "Cross-platform Text Expander written in Rust"
     homepage "https://github.com/schwa/swiftlint-autodetect"
-    url "https://github.com/schwa/simple-disk-benchmark-rs/releases/latest/download/swiftlint-autodetect.tar.gz"
+    url "https://github.com/schwa/swiftlint-autodetect/releases/latest/download/swiftlint-autodetect.tar.gz"
     sha256 "be3c18ea55c29255dbe4fc904f0ecbcc7397a9474ccd0d70f8629ffb8e1a7415"
     version "0.1.2"
 
