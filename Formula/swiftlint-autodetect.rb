220fba24d9675b06a9ae3a8fb8b7b5d3d6039cb4
Jonathan Wight <jwight@mac.com>
2024-06-29 22:07:21 -0400

    swiftlint-autodetect 0.1.4

--- Formula/swiftlint-autodetect.rb
+++ Formula/swiftlint-autodetect.rb
@@ -5,8 +5,8 @@ class SwiftlintAutodetect < Formula
     desc "Cross-platform Text Expander written in Rust"
     homepage "https://github.com/schwa/swiftlint-autodetect"
     url "https://github.com/schwa/swiftlint-autodetect/releases/latest/download/swiftlint-autodetect.tar.gz"
-    sha256 "223cc98a1009c6f6cbcc08de372a7733002d49bac01c6c0fe6839da1d259db92"
-    version "0.1.3"
+    sha256 "5464c0982fed2c8ce1616e121ff47a38ecd1683f02f40ca2e18c0a96b1146742"
+    version "0.1.4"
 
     def install
       bin.install "swiftlint-autodetect"
