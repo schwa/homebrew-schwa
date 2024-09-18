f6e6eb6a26019e197df92fbb2ae81a42a8e85fcc
Jonathan Wight <jwight@mac.com>
2024-09-18 08:14:16 -0700

    Fix class name

--- Formula/multigit.rb
+++ Formula/multigit.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class SwiftlintAutodetect < Formula
+class Multigit < Formula
     desc "A CLI tool to manage multiple Git repositories simultaneously."
     homepage "https://github.com/schwa/multigit-rs"
     url "https://github.com/schwa/multigit-rs/releases/latest/download/multigit-rs-0.1.1.tar.gz"
