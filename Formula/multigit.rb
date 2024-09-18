af12d5296adabca12caa242285663aee82eb3857
Jonathan Wight <jwight@mac.com>
2024-09-18 08:12:33 -0700

    Added multigit by hand.

---
+++ Formula/multigit.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class SwiftlintAutodetect < Formula
+    desc "A CLI tool to manage multiple Git repositories simultaneously."
+    homepage "https://github.com/schwa/multigit-rs"
+    url "https://github.com/schwa/multigit-rs/releases/latest/download/multigit-rs-0.1.1.tar.gz"
+    sha256 "fe12755045051c5e45bd73eac6bbaabae30f7d0b45159fdc78170e3504a84211"
+    version "0.1.1"
+
+    def install
+      bin.install "multigit"
+    end
+  end
