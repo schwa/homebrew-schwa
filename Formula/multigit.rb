b15d540905741e28244fabe75c976217014b91a2
Jonathan Wight <jwight@mac.com>
2024-09-18 21:37:04 -0700

    multigit 0.1.2

--- Formula/multigit.rb
+++ Formula/multigit.rb
@@ -5,8 +5,8 @@ class Multigit < Formula
     desc "A CLI tool to manage multiple Git repositories simultaneously."
     homepage "https://github.com/schwa/multigit-rs"
     url "https://github.com/schwa/multigit-rs/archive/refs/tags/0.1.1.tar.gz"
-    sha256 "fe12755045051c5e45bd73eac6bbaabae30f7d0b45159fdc78170e3504a84211"
-    version "0.1.1"
+    sha256 "69dc0291c70fa7713e15ef6bcaf2ce8edf6eed45dae0969fa74dcd44cd1b77ed"
+    version "0.1.2"
 
     def install
       bin.install "multigit-rs"
